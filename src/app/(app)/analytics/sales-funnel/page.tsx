d28e45c9f56497b829e8305d0bc31f160533d977
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2025-11-11 16:43:23 +0300

    Правки стилей контейнера дашбордов

--- src/app/(app)/analytics/sales-funnel/page.tsx
+++ src/app/(app)/analytics/sales-funnel/page.tsx
@@ -2,7 +2,7 @@ import { DatalensEmbed } from "@/components/analytics/DatalensEmbed";
 
 export default function Page() {
     return (
-        <div className="bg-white min-h-full flex-grow p-4">
+        <div className="bg-white min-h-full flex-grow p-4 flex flex-col">
             <DatalensEmbed />
         </div>
     );
