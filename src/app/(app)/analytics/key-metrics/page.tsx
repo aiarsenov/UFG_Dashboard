361e9ff2b3a0f049869b50cf0d3b223450fe3d7f
Vasilii Arsenov <vasiliiarsenov@example.com>
2025-12-16 23:40:55 +0300

    Обновлен embedId для страницы Ключевые показатели

--- src/app/(app)/analytics/key-metrics/page.tsx
+++ src/app/(app)/analytics/key-metrics/page.tsx
@@ -1,9 +1,11 @@
 import { DatalensEmbed } from "@/components/analytics/DatalensEmbed";
 
 export default function Page() {
+    // Используем новый embedId для "Ключевые показатели"
+    // ID: wgpyi46ojpbwg
     return (
         <div className="bg-white min-h-full flex-grow p-4 flex flex-col">
-            <DatalensEmbed />
+            <DatalensEmbed embedId="wgpyi46ojpbwg" />
         </div>
     );
 }
